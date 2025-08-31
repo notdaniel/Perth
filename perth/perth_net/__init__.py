43919e06aee9791a9c317954f6df1ab9c9f89005
Daniel Keywan Hollister <daniel.hollister@gmail.com>
2025-08-31 06:20:13 -0700

    Set up as a proper package with uv

--- perth/perth_net/__init__.py
+++ perth/perth_net/__init__.py
@@ -1,4 +1,10 @@
-from pkg_resources import resource_filename
-PREPACKAGED_MODELS_DIR = resource_filename(__name__, "pretrained")
+from pathlib import Path
+import importlib.resources as resources
+
+try:
+    PREPACKAGED_MODELS_DIR = str(resources.files(__name__) / "pretrained")
+except AttributeError:
+    import pkg_resources
+    PREPACKAGED_MODELS_DIR = pkg_resources.resource_filename(__name__, "pretrained")
 
 from .perth_net_implicit.perth_watermarker import PerthImplicitWatermarker
